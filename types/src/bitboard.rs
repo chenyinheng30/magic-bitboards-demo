d5a8a2ac1e6a8a2b8884c46454eb69a427a278d3
chenyinheng30 <chenyinheng30@outlook.com>
2024-07-29 16:44:20 +0800

    Add a Chinese chess test to bitboad.

--- types/src/bitboard.rs
+++ types/src/bitboard.rs
@@ -91,24 +91,28 @@ impl Iterator for BitBoardIter {
 #[macro_export]
 macro_rules! __bitboard {
     (
-        $a8:tt $b8:tt $c8:tt $d8:tt $e8:tt $f8:tt $g8:tt $h8:tt
-        $a7:tt $b7:tt $c7:tt $d7:tt $e7:tt $f7:tt $g7:tt $h7:tt
-        $a6:tt $b6:tt $c6:tt $d6:tt $e6:tt $f6:tt $g6:tt $h6:tt
-        $a5:tt $b5:tt $c5:tt $d5:tt $e5:tt $f5:tt $g5:tt $h5:tt
-        $a4:tt $b4:tt $c4:tt $d4:tt $e4:tt $f4:tt $g4:tt $h4:tt
-        $a3:tt $b3:tt $c3:tt $d3:tt $e3:tt $f3:tt $g3:tt $h3:tt
-        $a2:tt $b2:tt $c2:tt $d2:tt $e2:tt $f2:tt $g2:tt $h2:tt
-        $a1:tt $b1:tt $c1:tt $d1:tt $e1:tt $f1:tt $g1:tt $h1:tt
+        $a0:tt $b0:tt $c0:tt $d0:tt $e0:tt $f0:tt $g0:tt $h0:tt $i0:tt
+        $a9:tt $b9:tt $c9:tt $d9:tt $e9:tt $f9:tt $g9:tt $h9:tt $i9:tt
+        $a8:tt $b8:tt $c8:tt $d8:tt $e8:tt $f8:tt $g8:tt $h8:tt $i8:tt
+        $a7:tt $b7:tt $c7:tt $d7:tt $e7:tt $f7:tt $g7:tt $h7:tt $i7:tt
+        $a6:tt $b6:tt $c6:tt $d6:tt $e6:tt $f6:tt $g6:tt $h6:tt $i6:tt
+        $a5:tt $b5:tt $c5:tt $d5:tt $e5:tt $f5:tt $g5:tt $h5:tt $i5:tt
+        $a4:tt $b4:tt $c4:tt $d4:tt $e4:tt $f4:tt $g4:tt $h4:tt $i4:tt
+        $a3:tt $b3:tt $c3:tt $d3:tt $e3:tt $f3:tt $g3:tt $h3:tt $i3:tt
+        $a2:tt $b2:tt $c2:tt $d2:tt $e2:tt $f2:tt $g2:tt $h2:tt $i2:tt
+        $a1:tt $b1:tt $c1:tt $d1:tt $e1:tt $f1:tt $g1:tt $h1:tt $i1:tt
     ) => {
         $crate::__bitboard! { @__inner
-            $a1 $b1 $c1 $d1 $e1 $f1 $g1 $h1
-            $a2 $b2 $c2 $d2 $e2 $f2 $g2 $h2
-            $a3 $b3 $c3 $d3 $e3 $f3 $g3 $h3
-            $a4 $b4 $c4 $d4 $e4 $f4 $g4 $h4
-            $a5 $b5 $c5 $d5 $e5 $f5 $g5 $h5
-            $a6 $b6 $c6 $d6 $e6 $f6 $g6 $h6
-            $a7 $b7 $c7 $d7 $e7 $f7 $g7 $h7
-            $a8 $b8 $c8 $d8 $e8 $f8 $g8 $h8
+            $a1 $b1 $c1 $d1 $e1 $f1 $g1 $h1 $i1
+            $a2 $b2 $c2 $d2 $e2 $f2 $g2 $h2 $i2
+            $a3 $b3 $c3 $d3 $e3 $f3 $g3 $h3 $i3
+            $a4 $b4 $c4 $d4 $e4 $f4 $g4 $h4 $i4
+            $a5 $b5 $c5 $d5 $e5 $f5 $g5 $h5 $i5
+            $a6 $b6 $c6 $d6 $e6 $f6 $g6 $h6 $i6
+            $a7 $b7 $c7 $d7 $e7 $f7 $g7 $h7 $i7
+            $a8 $b8 $c8 $d8 $e8 $f8 $g8 $h8 $i8
+            $a9 $b9 $c9 $d9 $e9 $f9 $g9 $h9 $i9
+            $a0 $b0 $c0 $d0 $e0 $f0 $g0 $h0 $i0
         }
     };
     (@__inner $($occupied:tt)*) => {{
@@ -135,7 +139,7 @@ macro_rules! __bitboard {
         )
     };
     ($($token:tt)*) => {
-        compile_error!("Expected 64 squares")
+        compile_error!("Expected 90 squares")
     };
 }
 pub use __bitboard as bitboard;
@@ -159,5 +163,61 @@ impl std::fmt::Debug for BitBoard {
         } else {
             write!(f, "BitBoard({:#018X})", self.0)
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_bitboard_macro0() {
+        let board = bitboard! {
+            . . . . . . . . .
+            . . . . . . . . .
+            . . . . . . . . .
+            X . X . X . X . X
+            . . . . . . . . .
+            . . . . . . . . .
+            X . X . X . X . X
+            . . . . . . . . .
+            . . . . . . . . .
+            . . . . . . . . .
+        };
+        assert!(board.0 == 0x5540000AA8000000u128, "fail with board = {:?}", board);
+    }
+
+    #[test]
+    fn test_bitboard_macro1() {
+        let board = bitboard! {
+            . . . . . . . . .
+            . . . . . . . . .
+            . . . . . . . . .
+            X . X . . . X . .
+            . . . . X . . . X
+            . . . . . . . . .
+            X . X . X . X . .
+            . . . . . . . . .
+            . . . . . . . . .
+            . . . . . . . . .
+        };
+        assert!(board.0 == 0x11620002A8000000u128, "fail with board = {:?}", board);
+    }
+
+    #[test]
+    fn test_bitboard_macro2() {
+        let board = bitboard! {
+            X . . . . . . . X
+            . . . . . . . . .
+            . . . . . . . . .
+            . . . . . . . . .
+            . . . . . . . . .
+            . . . . . . . . .
+            . . . . . . . . .
+            . . . . . . . . .
+            X . . . . . . . .
+            . . . . . . . . X
+        };
+        assert!(board.0 == 0x20200000000000000000300u128, "fail with board = {:?}", board);
     }
 }
